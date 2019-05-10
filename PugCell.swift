67aca4a3a612f11e7b95a6f13b14f86c9b92556e
Jack Kelley <jackkelley@college.harvard.edu>
2019-05-10 10:27:53 -0400

    Version 1.0 Finished

---
+++ PugCell.swift
@@ -0,0 +1,9 @@
+//
+//  PugCell.swift
+//  Pugs
+//
+//  Created by Jack Kelley on 5/6/19.
+//  Copyright © 2019 Jack Kelley. All rights reserved.
+//
+
+import Foundation
---
+++ Pugs/LikeButton.swift
@@ -0,0 +1,13 @@
+//
+//  LikeButton.swift
+//  
+//
+//  Created by Jack Kelley on 5/8/19.
+//
+
+import UIKit
+
+class LikeButton: UIButton {
+    var indexPath:NSIndexPath?
+    var url:String?
+}
